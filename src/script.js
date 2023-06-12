aedc275324699f2fca188c95961260a4c8c7474f
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-12 18:40:34 -0300

    if condition

--- src/script.js
+++ src/script.js
@@ -2,6 +2,10 @@ function preencher() {
     const tbody = document.getElementById('tbody');
     const nomeValue = document.getElementById('nome-anime').value;
     const descValue = document.getElementById('desc-anime').value;
+    if(nomeValue === '' || descValue === ''){
+        alert('Nome ou descrição estão vazios')
+        return
+    }
     tbody.innerHTML+= `<tr><td>${nomeValue}</td><td>${descValue}</td></tr>`
     document.getElementById('nome-anime').value = '';
     document.getElementById('desc-anime').value = '';
