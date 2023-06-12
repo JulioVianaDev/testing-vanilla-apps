40167554dda2439ed41f7cfb13555727a4094b64
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-12 16:24:08 -0300

    criando primeiro teste

---
+++ src/elementos.test.js
@@ -0,0 +1,27 @@
+import { fireEvent, getByText } from '@testing-library/dom'
+import '@testing-library/jest-dom/extend-expect'
+import { JSDOM } from 'jsdom'
+import fs from 'fs'
+import path from 'path'
+
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+
+let dom
+let container
+
+describe('index.html', () => {
+  beforeEach(() => {
+    // https://github.com/jsdom/jsdom#executing-scripts
+    const options = {
+      resources: 'usable',
+      runScripts: 'dangerously',
+    };
+    dom = new JSDOM(html,options)
+    container = dom.window.document.body
+  })
+
+  it('renders a heading element', () => {
+    expect(container.querySelector('h1')).not.toBeNull()
+    expect(getByText(container, 'Testando Apps')).toBeInTheDocument()
+  })
+})
